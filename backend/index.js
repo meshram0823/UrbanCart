6fdd1445009746e6ed284ba9bcc292cc3ba05c2f
meshram0823 <pranjalmeshram2317@gmail.com>
2024-09-07 23:12:52 +0530

    Trying

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,7 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+
 dotenv.config();
 const port = process.env.PORT || 5000;
 
@@ -23,19 +24,23 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 
-app.use("/", () => {
-  return res.json("Hello");
-});
 app.use("/api/users", userRoutes);
 app.use("/api/category", categoryRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/upload", uploadRoutes);
-app.use("/api/orders",orderRoutes);
+app.use("/api/orders", orderRoutes);
+
 app.get("/api/config/paypal", (req, res) => {
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
 });
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
